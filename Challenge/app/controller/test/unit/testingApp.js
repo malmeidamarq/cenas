2b4ba84f11e9250cde1542c6ee50e761f6c2aeb5
Maria Almeida <malmeida@e-goi.com>
2019-11-09 21:57:06 +0000

    Create testingApp.js

---
+++ Challenge/app/controller/test/unit/testingApp.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Unit Test', function(){
+    beforeEach(module('app'));
+
+    describe('Testing Controller', function () {
+        var myctrl,  scope, httpMock;
+        // beforeEach(inject(function($controller, $rootScope, $httpBackend){ //instantiate controller using $controller service and inject $rootScope service
+        //     scope = $rootScope.$new();
+        //     httpMock = $httpBackend;
+        //     myctrl = $controller('indexCtrl', {
+        //     $scope : scope,
+        //     $httpBackend: httpMock
+        //     });
+        // }));
+
+        // it("should make a GET call to data.json", function () {
+        //     console.log("********** SERVICE ***********");
+        //     httpMock.expectGET("../../../json/apps.json").respond("Response found!");
+        //     httpMock.flush();
+        // });
+
+
+        it('should select channels category', function () {
+
+        });
+        
+    });
+});
